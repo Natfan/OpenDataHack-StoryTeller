afafd3aecb7c397e907cf20e57c9c4f1a2b50663
ChrisFairless <chrisfairless@hotmail.com>
2017-03-04 22:57:18 +0000

    Add weather data download function and NetCDF variable extraction. All still in progress

--- weather_data/get_weather_data.py
+++ weather_data/get_weather_data.py
@@ -10,21 +10,41 @@ adapted from ECMWF template
 """
 
 from ecmwfapi import ECMWFDataServer
+import datetime
+import string
+
+def get_ECMWF_data(year,month,day,params,):
+
+    # Set up the date variables - add one day to get the range we'd like
+    one_day = datetime.timedelta(days=1)
+    date_start = datetime.date(year,month,day)
+    date_end = date_start + one_day
+    date_str = str(date_start) + "/to/" + str(date_end)
+
+    # Set up the strings for the parameters we want
+    params_str = string.join(params,"")
+
+    date_start_str = date_start.strftime("%Y-%m-%d")
+    out_filename = "./data/ERAInt_"+date_start_str+".nc"
+
+    # Create a server object
+    server = ECMWFDataServer()
     
-server = ECMWFDataServer()
-    
-server.retrieve({
-    "class": "ei",
-    "dataset": "interim",
-    "date": "1988-09-08/to/1988-09-09",
-    "expver": "1",
-    "grid": "0.75/0.75",
-    "levtype": "sfc",
-    "param": "165.128/166.128/167.128",
-    "step": "0",
-    "stream": "oper",
-    "time": "00:00:00/06:00:00/12:00:00/18:00:00",
-    "type": "an",
-    "format": "netcdf",
-    "target": "temp.nc",
-})
+    # Get the data
+    server.retrieve({
+        "class": "ei",
+        "dataset": "interim",
+        "date": date_str,
+        "expver": "1",
+        "grid": "0.75/0.75",
+        "levtype": "sfc",
+        "param": params_str,
+        "step": "0",
+        "stream": "oper",
+        "time": "00:00:00/06:00:00/12:00:00/18:00:00",
+        "type": "an",
+        "format": "netcdf",
+        "target": out_filename
+    })
+
+get_ECMWF_data( 1998, 9, 8, "167.128")
