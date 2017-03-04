506bdf696109aa53021eebd10de5036bf544b260
Nathan Windisch <nathan@windisch.co.uk>
2017-03-04 22:59:00 +0000

    added frank and did some coding

--- prog.py
+++ prog.py
@@ -11,9 +11,6 @@ import nltk;
 print("version 2");
 
 #text = 'This is a sentence that contains some words. These words are sometimes long, but mostly short. I like words, some may even say that I have the best words!';
-text = 'could i possibly have eaten your cheese?'
-text = nltk.word_tokenize(text);
-result = nltk.pos_tag(text);
 
 dt = [];
 vb = [];
@@ -27,45 +24,53 @@ ignore = [];
 
 #result = [i for i in result if i[0].lower == 'words']
 
-for item, index in result:
-	if index == 'DT' or index == 'PRP$':
-		dt.append(item);
-	if index == 'VB' or index == 'VBD' or index == 'VBG' or index == 'VBN' or index == 'VBP' or index == 'VBZ':
-		vb.append(item);
-	if index == 'NN' or index == 'NNS' or index == 'NNP' or index == 'NNPS' or index == 'PRP':
-		nn.append(item);
-	if index == 'WDT':
-		wdt.append(item);
-	if index == 'RB' or index == 'RBR' or index == 'RBS':
-		rb.append(item);
-	if index == 'CC' or index == 'IN':
-		conj.append(item);
-	if index == 'JJ' or index == 'JJR' or index == 'JJS':
-		jj.append(item);
-	if index == 'IN':
-		conj.append(item);
-	if index == '?' or index == 'WRB' or ignore == 'MD':
-		ignore.append(item);
-	if index == ',' or index == '.' or index == '!':
-		gmr.append(item);
-print("");
-print "Determiner", dt;
-print("");
-print "Verb", vb;
-print("");
-print "Noun", nn;
-print("");
-print "WH-Determiner", wdt;
-print("");
-print "Adverb", rb;
-print("");
-print "Adjective", jj;
-print("");
-print "Conjuction", conj;
-print("");
-print "Grammar", gmr;
-print("");
-print "Ignore", ignore;
-print("");
+verbose = False;
 
-print result;
+def getWordTypes(text):
+	text = nltk.word_tokenize(text);
+	words = nltk.pos_tag(text);
+	if verbose == True:
+		for item, index in words:
+			if index == 'DT' or index == 'PRP$':
+				dt.append(item);
+			if index == 'VB' or index == 'VBD' or index == 'VBG' or index == 'VBN' or index == 'VBP' or index == 'VBZ':
+				vb.append(item);
+			if index == 'NN' or index == 'NNS' or index == 'NNP' or index == 'NNPS' or index == 'PRP':
+				nn.append(item);
+			if index == 'WDT':
+				wdt.append(item);
+			if index == 'RB' or index == 'RBR' or index == 'RBS':
+				rb.append(item);
+			if index == 'CC' or index == 'IN':
+				conj.append(item);
+			if index == 'JJ' or index == 'JJR' or index == 'JJS':
+				jj.append(item);
+			if index == 'IN':
+				conj.append(item);
+			if index == '?' or index == 'WRB' or ignore == 'MD':
+				ignore.append(item);
+			if index == ',' or index == '.' or index == '!':
+				gmr.append(item);
+		print("");
+		print "Determiner", dt;
+		print("");
+		print "Verb", vb;
+		print("");
+		print "Noun", nn;
+		print("");
+		print "WH-Determiner", wdt;
+		print("");
+		print "Adverb", rb;
+		print("");
+		print "Adjective", jj;
+		print("");
+		print "Conjuction", conj;
+		print("");
+		print "Grammar", gmr;
+		print("");
+		print "Ignore", ignore;
+		print("");
+	else:
+		print words;
+	
+getWordTypes("why hello my fellow human. i am not a robot, i am alive! a real boy, i do declare my variables");
