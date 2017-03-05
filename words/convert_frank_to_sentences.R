a942a42bc790b9d86927f5e469fbaf85682cb064
ChrisFairless <chrisfairless@hotmail.com>
2017-03-05 11:11:58 +0000

    script to convert frankenstein into nice readable lines

---
+++ words/convert_frank_to_sentences.R
@@ -0,0 +1,15 @@
+
+#
+
+x1 <- read.csv("frank.txt", sep="\n")
+x2 <- paste(paste(x1[[1]], sep=" "),collapse=" ")
+splitstrings <- c("\\.","!","?",":",";")
+splitstrings <- c(splitstrings, paste(splitstrings," ",sep=""))
+x3 <- strsplit(as.character(x2[[1]]), splitstrings)
+x3 <- gsub("\\n"," ",x3[[1]])
+x3 <- gsub('"',"", x3)
+
+trim.leading <- function (x)  { return(sub("^\\s+", "", x)) }
+x4 <- trim.leading(x3)
+
+write.csv(x4,"frank.csv",quote=FALSE,row.names=FALSE)
